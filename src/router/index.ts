d2e186abdbc5845beeed82295402f75ef4077d4f
waid00 <davidwais14@gmail.com>
2025-04-19 19:58:18 +0200

    fixed home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
+// Import your pages (filenames must match!)
+import Home from '@/pages/Home.vue'
+import Generate from '@/pages/Generate.vue'
+
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/generate',
     name: 'Generate',
-    component: () => import('@/pages/Generate.vue')
+    component: Generate
   }
 ]
 
